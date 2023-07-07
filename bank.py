cd90d562b55f12a7b87844fd68e6ee36a4f158f3
Arjun <93868509+Arjun100100@users.noreply.github.com>
2023-07-07 16:59:11 +0530

    bank.py

--- bank.py
+++ bank.py
@@ -1,5 +1,8 @@
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 import streamlit as st
+import matplotlib.ticker as mtick
 
 def question_1(df):
   total_roi_by_bank = df.groupby('Bank_Name')['Return_on_Investment'].mean().idxmax()
@@ -11,8 +14,19 @@ def question_2(df):
   st.write("Average Interest Rate Variation Among banks:")
   st.write(roi_by_investment_type)
 
-
 def question_3(df):
+  plt.figure(figsize=(8, 6))
+  sns.scatterplot(x="Capital_Invested", y="Current_Value_of_Investment", data=df)
+  plt.xlabel("Capital Invested")
+  plt.ylabel("Current Value of Investment")
+  plt.title("Correlation between Capital Invested and Current Value")
+  st.pyplot()
+
+  correlation_coefficient = df["Capital_Invested"].corr(df["Current_Value_of_Investment"])
+  st.write("Correlation Coefficient between Capital Invested and Current Value:")
+  st.write(correlation_coefficient)
+
+def question_4(df):
   investment_type_counts = df.pivot_table(index="Bank_Name", columns="Investment_Type", values="Customer_ID", aggfunc="count")
   st.write("Investment Type Counts by Bank:")
   st.write(investment_type_counts)
@@ -22,7 +36,31 @@ def question_3(df):
   st.write(most_popular_investment_by_bank)
 
 
-def question_4(df):
+def question_5(df):
+  sns.set_style("white")
+  plt.figure(figsize=(10, 6))
+  sns.lineplot(x="Investment_Year", y="Interest_Rate", hue="Bank_Name", data=df)
+  plt.xlabel("Investment Year")
+  plt.ylabel("Interest Rate")
+  plt.title("Trend in Interest Rates over the Years for Each Bank")
+  plt.legend(title='Bank Name', bbox_to_anchor=(1.05, 1), loc='upper left')
+  plt.gca().yaxis.set_major_formatter(mtick.PercentFormatter(1.0))
+  st.pyplot()
+
+def question_6(df):
+  bins = [0, 10000, 20000, 40000, 60000, 80000, 100000, float('inf')]
+  labels = ['<10k', '10k-20k', '20k-40k', '40k-60k', '60k-80k', '80k-100k', '100k+']
+  df['Capital_Investment_Group'] = pd.cut(df['Capital_Invested'], bins=bins, labels=labels)
+  average_interest_rate = df.groupby('Capital_Investment_Group')['Interest_Rate'].mean()
+  plt.figure(figsize=(8, 6))
+  average_interest_rate.plot(kind='bar', color='skyblue')
+  plt.xlabel("Capital Invested")
+  plt.ylabel("Average Interest Rate")
+  plt.title("Average Interest Rate for Different Capital Investment Groups")
+  plt.grid(axis='y')
+  st.pyplot()
+
+def question_7(df):
   volatility_by_investment_type = df.groupby('Investment_Type')['Return_on_Investment'].std()
   highest_volatility_investment_type = volatility_by_investment_type.idxmax()
   highest_volatility_std_dev = volatility_by_investment_type.max().round(3)
@@ -54,7 +92,7 @@ def main():
         st.subheader("Step 2: Select Questions")
         questions = st.multiselect(
             "Select the questions to answer:",
-            [1, 2, 3, 4],
+            [1, 2, 3, 4, 5, 6, 7],
             format_func=lambda q: f"Question {q}",
         )
 
